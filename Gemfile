d3e7b51c7c1176e050db429e9c54d400de1bd25d
Michael Pierce <michael@digiquatics.com>
2017-04-08 08:54:19 -0600

    sitemap

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ group :development do
   gem 'listen', '~> 3.0.5'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'sitemap_generator'
 end
 
 group :production do
---
+++ config/sitemap.rb
@@ -0,0 +1,12 @@
+SitemapGenerator::Sitemap.default_host = "https://www.cgs-llc.net"
+SitemapGenerator::Sitemap.create do
+  add '/'
+  add '/contact'
+  add '/services'
+  add '/about'
+  add '/free_quote'
+  add '/testimonials'
+  add '/taxidermy'
+  add '/privacy_policy'
+  add 'terms_of_service'
+end
